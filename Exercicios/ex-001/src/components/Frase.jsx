6c8357d3d765bf85753b8db00347efa16fc9cf3f
Alex Simão de Souza <126366771+AlexSimao@users.noreply.github.com>
2023-11-04 15:39:16 -0300

    Utilizado componentes dentro de componentes.

---
+++ Exercicios/ex-001/src/components/Frase.jsx
@@ -0,0 +1,9 @@
+function Frase() {
+  return (
+    <div>
+      <p>Frase de Conteudo.</p>
+    </div>
+  );
+}
+
+export default Frase;
--- Exercicios/ex-001/src/components/HelloWorld.jsx
+++ Exercicios/ex-001/src/components/HelloWorld.jsx
@@ -1,7 +1,11 @@
+import Frase from "./Frase";
+
 function HelloWorld() {
   return (
     <div>
+      <Frase />
       <h3>Criando Componente.</h3>
+      <Frase />
     </div>
   );
 }
