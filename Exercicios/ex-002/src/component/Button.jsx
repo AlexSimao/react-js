7988037491de1bebf0af06bf7b31089705200d8c
Alex Simão de Souza <126366771+AlexSimao@users.noreply.github.com>
2023-11-14 19:09:39 -0300

    Adicionando defaultProp e propType no Button

--- Exercicios/ex-002/src/component/Button.jsx
+++ Exercicios/ex-002/src/component/Button.jsx
@@ -1,5 +1,16 @@
+import PropType from "prop-types";
+
 function Button({ text, event }) {
   return <button onClick={event}>{text}</button>;
 }
+
+Button.defaultProps = {
+  text: "Button",
+};
+
+Button.propTypes = {
+  text: PropType.string,
+  event: PropType.func,
+};
 
 export default Button;
