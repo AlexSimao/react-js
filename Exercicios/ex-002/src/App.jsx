7c7fcbaaae933178d9a9fab3b73eba7e7f7f5811
Alex Simão de Souza <126366771+AlexSimao@users.noreply.github.com>
2023-11-14 22:35:41 -0300

    Renderizando elementos por condicionais no React

--- Exercicios/ex-002/src/App.jsx
+++ Exercicios/ex-002/src/App.jsx
@@ -1,14 +1,12 @@
 import "./App.css";
+import Condicional from "./component/Condicional";
 import Evento from "./component/Evento";
 import Form from "./component/Form";
 
 function App() {
   return (
     <>
-      <div>
-        <Evento />
-      </div>
-      <Form />
+      <Condicional />
     </>
   );
 }
---
+++ Exercicios/ex-002/src/component/Condicional.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import Button from "./Button";
+
+function Condicional() {
+  const [email, setEmail] = useState("");
+  const [userEmail, setUserEmail] = useState();
+
+  function btnEnviar(e) {
+    e.preventDefault();
+    setUserEmail(email);
+  }
+
+  return (
+    <div>
+      <h2>Cadastre seu E-mail:</h2>
+      <form>
+        <input
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          type="email"
+          name="email"
+          id="email"
+        />
+        <Button
+          text="Enviar E-mail"
+          event={btnEnviar}
+        />
+      </form>
+      {userEmail && (
+        <div>
+          <p>O Email do Usuário e {userEmail}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Condicional;
